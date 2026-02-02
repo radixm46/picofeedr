bd0f92f97f7a664148f06afe67606cf14db1c25f
radixm46 <radixm46@gmail.com>
2026-02-02 12:13:55 +0900

    refactor: cargo fmt applied

--- src/feed/mod.rs
+++ src/feed/mod.rs
@@ -4,6 +4,6 @@ mod api;
 mod identity;
 mod reconcile;
 
-pub use api::{diff_config_vs_db, render_feed_list, FeedConfigDiffResponse, FeedListResponse};
+pub use api::{FeedConfigDiffResponse, FeedListResponse, diff_config_vs_db, render_feed_list};
 pub use identity::feed_key_from_url;
 pub use reconcile::{reconcile_feeds, reconcile_feeds_with_conn};
